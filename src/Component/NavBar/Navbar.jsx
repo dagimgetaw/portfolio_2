6039fc5aab6c739ee3c8f851b5dff8500e36ff4f
Dagim <dagimgetaW27@gmail.com>
2024-08-13 00:11:38 +0300

    navbar

---
+++ src/Component/NavBar/Navbar.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import "./navbar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(true);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <nav>
+      <div className="navbar">
+        <div className="nav_logo">
+          <h1>
+            <a href="">dagim</a>
+          </h1>
+        </div>
+        <div className="nav_item">
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Service</a>
+            </li>
+            <li>
+              <a href="">Skill</a>
+            </li>
+            <li>
+              <a href="">Portfolio</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="nav_toggle" onClick={handleToggle}>
+          {toggle ? <FaBars /> : <FaTimes />}
+        </div>
+      </div>
+      {toggle ? (
+        ""
+      ) : (
+        <div className="nav_item_toggle">
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Service</a>
+            </li>
+            <li>
+              <a href="">Skill</a>
+            </li>
+            <li>
+              <a href="">Portfolio</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
