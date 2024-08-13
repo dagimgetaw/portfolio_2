0ae1bbb697d0032a0358521c5cf6d8b2044fd26f
Dagim <dagimgetaW27@gmail.com>
2024-08-13 19:16:51 +0300

    service

---
+++ src/Component/Service/Service.jsx
@@ -0,0 +1,50 @@
+import "./service.css";
+import { FaGlobe, FaMobileAlt, FaChalkboardTeacher } from "react-icons/fa";
+
+const Service = () => {
+  return (
+    <div className="service">
+      <div className="service_form">
+        <h2>WHAT I CAN DO FOR YOU?</h2>
+        <p>
+          I have over 3 years of extensive experience in web development, with a
+          strong understanding and expertise in full-stack web development.{" "}
+        </p>
+      </div>
+      <div className="service_container">
+        <div className="box">
+          <FaGlobe className="icon" />
+          <p className="service_title">
+            <span>WEB DESIGN</span>
+          </p>
+          <p className="service_para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur
+            adipiscing elit
+          </p>
+        </div>
+        <div className="box">
+          <FaMobileAlt className="icon" />
+          <p className="service_title">
+            <span>APP DEVELOPMENT</span>
+          </p>
+          <p className="service_para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur
+            adipiscing elit
+          </p>
+        </div>
+        <div className="box">
+          <FaChalkboardTeacher className="icon" />
+          <p className="service_title">
+            <span>TRAINER</span>
+          </p>
+          <p className="service_para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur
+            adipiscing elit
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
