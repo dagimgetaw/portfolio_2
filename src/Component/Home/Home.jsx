3050ac7013494d6a67048a2028a73819303ae40e
Dagim <dagimgetaW27@gmail.com>
2024-08-13 19:18:05 +0300

    home update

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,27 @@
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="title">
+        <h2>
+          <span>Dagim Getaw</span>, a software engineer.
+        </h2>
+        <p>
+          I have over 3 years of extensive experience in web development, with a
+          strong understanding and expertise in full-stack web development.{" "}
+        </p>
+        <div className="link">
+          <button>
+            <a href="">View Resume</a>
+          </button>
+          <button>
+            <a href="">Hire me</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
