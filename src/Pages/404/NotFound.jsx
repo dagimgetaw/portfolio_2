7bbcbed25e06cd344e009595eceb0456eed08a87
Dagim <dagimgetaW27@gmail.com>
2024-08-16 02:25:49 +0300

    add custom 404 page

--- src/Pages/404/NotFound.jsx
+++ src/Pages/404/NotFound.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import Navbar from "../../Component/NavBar/Navbar";
+import Links from "../../Component/Link/Links";
 import "./notfound.css";
 
 // eslint-disable-next-line react/prop-types
-const NotFound = ({ color }) => {
+const NotFound = ({ color, setColor }) => {
   return (
     <>
+      <Navbar />
+      <Links color={color} setColor={setColor} />
       <div className={color ? "notfound dark_theme" : "notfound light_theme"}>
         <h2>404</h2>
         <p>Page not found</p>
