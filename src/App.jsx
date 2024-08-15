9981a56b6a315a115ea7576745d167b4ea55ee7b
Dagim <dagimgetaW27@gmail.com>
2024-08-16 02:27:05 +0300

    use react router

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-import Mainlayout from "./Layout/Mainlayout";
+// import Mainlayout from "./Layout/Mainlayout";
 import NotFound from "./Pages/404/NotFound";
+import Resume from "./Pages/Resume/Resume";
 
 const App = () => {
   const [color, setColor] = useState(false);
@@ -15,11 +16,15 @@ const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route
-        path="/"
-        element={<Mainlayout color={color} setColor={setColor} />}
+      // path="/"
+      // element={<Mainlayout color={color} setColor={setColor} />}
       >
         <Route index element={<HomePage color={color} setColor={setColor} />} />
-        <Route path="*" element={<NotFound color={color} />} />
+        <Route
+          path="*"
+          element={<NotFound color={color} setColor={setColor} />}
+        />
+        <Route path="/resume" element={<Resume />} />
       </Route>
     )
   );
